759400627b613d2c6d62c060881ea4a159de5a4b
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-10-24 03:37:10 -0300

    1

--- js/data.js
+++ js/data.js
@@ -7,4 +7,5 @@ data1.setDate(data1.getDate());
 let ano = data1.getFullYear();
 let dia = data1.getDate(); if(dia<10){dia='0'+dia};
 let mes = (data1.getMonth()+1); if(mes<10){mes='0'+mes};
+
 novaData.innerHTML = (((`Você acessou meu currículo em ${dia}/${mes}/${ano}.`)));
